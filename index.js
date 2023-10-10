c10a57b7f9174fc9a5ef57e7141aee36092b4686
Ruiwen523 <ruiwen523@gmail.com>
2023-10-10 22:48:02 +0800

    Vue.js練習。

--- index.js
+++ index.js
@@ -1,15 +1,80 @@
 const vm = Vue.createApp({
-    data() {
-        return {
-            message: 'Hello Vue 3.0!',
-            name: '008JS',
-            price: 100,
-            quantity: 10
+  // 狀態屬性
+  data() {
+    return {
+      message: "Hello ",
+      name: "008JS",
+      price: 100,
+      quantity: 10,
+      twd: 1000,
+      jpy: 1,
+      customId: 'item-id-1',
+      isBtnDisabled: true,
+      picked: 1,
+      checkedNames: [],
+      isChecked: true,
+      selected: '',
+      msg1: 'Hello ',
+      msg2: 'Hello ',
+      num1: 0,
+      num2: 0,
+      text: '<b>Hello</b>',
+      isActive: true,
+      hasError: true
+    };
+  },
+  // 方法屬性
+  methods: {
+    subtotal: function () {
+        return this.price * this.quantity;
+    },    
+    subtotal_ES6 () {
+        // ES6
+        return this.price * this. quantity;
+    },
+    twd2jpy () {
+        this.jpy = Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
+    },
+    jpy2twd () {
+        this.twd = Number.parseFloat(Number(this.jpy) * 0.278).toFixed(3);
+    },    
+  },
+  // 計算屬性
+  computed: {
+    subtotal_cp: function () {
+        return this.price * this.quantity;
+    },
+    jpy_cp: {
+        get () {
+            return Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
+        },
+        set (val) {
+            this.twd = Number.parseFloat(Number(val) * 0.278).toFixed(3);
         }
+    },
+    usd_cp: {
+        get () {
+            return Number.parseFloat(Number(this.twd) / 28.540).toFixed(3);
+        },
+        set (val) {
+            this.twd = Number.parseFloat(Number(val) * 28.540).toFixed(3);
+        }
+    },
+    sum () {
+        return this.num1 + this.num2;
     }
-});//.mount('#app');
-
+  },
+}); //.mount('#app');
 // document.addEventListener("DOMContentLoaded", () => {
 //     // 需等待DOM載入完成後再綁定app物件
 //     vm.mount('#app');
 // })
+
+const vm2 = Vue.createApp({
+  template: `<div> {{ greeting }} 好棒棒!</div>`,
+  data() {
+    return {
+      greeting: "Hello Vue.js!",
+    };
+  },
+});
--- readme.md
+++ readme.md
@@ -8,15 +8,19 @@
 
 開啟VSCode至專案目錄  
 npm install vue  
-npm install -g @vue/cli
+npm install -g @vue/cli  
 npm -i vue@{version}  => 3.3.4
 > 徹底弄懂npm create/init指令在說什麼  
 > ref: https://juejin.cn/post/7133992076735152136  
 `npm create`是`npm init`的別名   用来初始化项目的 `package.json`
 
-> cd \<your-project-name>  
+
+```cd \<your-project-name>  
 > npm install  
-> npm run dev  
 > npm run build
->
-> Vue-Froundend
+> npm run dev
+```
+
+Vue-Froundend
+## Vue 前端練習筆記
+如果是同樣的重複計算，用computed 來處理會比methods 效能來得好， 但要注意的是computed 屬性中的function 無法使用參數，若有需要帶入參數的情況，還是只能使用methods 來處理。
