f9236013e7dd0c13a599696649bfb5f1e94a669a
Ruiwen523 <ruiwen523@gmail.com>
2023-10-11 23:03:24 +0800

    Vue.js練習。

--- index.js
+++ index.js
@@ -8,61 +8,87 @@ const vm = Vue.createApp({
       quantity: 10,
       twd: 1000,
       jpy: 1,
-      customId: 'item-id-1',
+      customId: "item-id-1",
       isBtnDisabled: true,
       picked: 1,
       checkedNames: [],
       isChecked: true,
-      selected: '',
-      msg1: 'Hello ',
-      msg2: 'Hello ',
+      selected: "",
+      msg1: "Hello ",
+      msg2: "Hello ",
       num1: 0,
       num2: 0,
-      text: '<b>Hello</b>',
+      text: "<b>Hello</b>",
       isActive: true,
-      hasError: true
+      hasError: true,
+      count: 0,
+      amount: 50,
+      msg: [],
     };
   },
   // 方法屬性
   methods: {
     subtotal: function () {
-        return this.price * this.quantity;
-    },    
-    subtotal_ES6 () {
-        // ES6
-        return this.price * this. quantity;
+      return this.price * this.quantity;
     },
-    twd2jpy () {
-        this.jpy = Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
+    subtotal_ES6() {
+      // ES6
+      return this.price * this.quantity;
     },
-    jpy2twd () {
-        this.twd = Number.parseFloat(Number(this.jpy) * 0.278).toFixed(3);
-    },    
+    twd2jpy() {
+      this.jpy = Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
+    },
+    jpy2twd() {
+      this.twd = Number.parseFloat(Number(this.jpy) * 0.278).toFixed(3);
+    },
+    ChangeIsActive() {
+      this.isActive = this.isActive == true ? false : true;
+    },
+    ChangehasError() {
+      this.hasError = this.hasError == true ? false : true;
+    },
+    alert(val) {
+      alert(val);
+      this.msg.push(val);
+      window.setTimeout(() => {
+        this.msg = [];
+      }, 3000);
+    },
+    plus() {
+      this.count++;
+    },
+    plus2(event) {
+      console.log(event.target.tagName);
+    },
+    plus3(amount, event) {
+      console.log(event.target.tagName);
+      this.count += amount;
+    },
   },
   // 計算屬性
   computed: {
     subtotal_cp: function () {
-        return this.price * this.quantity;
+      return this.price * this.quantity;
     },
     jpy_cp: {
-        get () {
-            return Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
-        },
-        set (val) {
-            this.twd = Number.parseFloat(Number(val) * 0.278).toFixed(3);
-        }
+      get() {
+        return Number.parseFloat(Number(this.twd) / 0.278).toFixed(3);
+      },
+      set(val) {
+        this.twd = Number.parseFloat(Number(val) * 0.278).toFixed(3);
+      },
     },
     usd_cp: {
-        get () {
-            return Number.parseFloat(Number(this.twd) / 28.540).toFixed(3);
-        },
-        set (val) {
-            this.twd = Number.parseFloat(Number(val) * 28.540).toFixed(3);
-        }
+      get() {
+        return Number.parseFloat(Number(this.twd) / 28.54).toFixed(3);
+      },
+      set(val) {
+        this.twd = Number.parseFloat(Number(val) * 28.54).toFixed(3);
+      },
     },
-    sum () {
-        return this.num1 + this.num2;
-    }
+    sum() {
+      return this.num1 + this.num2;
+    },
   },
 }); //.mount('#app');
 // document.addEventListener("DOMContentLoaded", () => {
--- readme.md
+++ readme.md
@@ -24,3 +24,54 @@ npm -i vue@{version}  => 3.3.4
 Vue-Froundend
 ## Vue 前端練習筆記
 如果是同樣的重複計算，用computed 來處理會比methods 效能來得好， 但要注意的是computed 屬性中的function 無法使用參數，若有需要帶入參數的情況，還是只能使用methods 來處理。
+
+## 常見Vue實體選用Options
+- 狀態(資料)屬性
+  - data(){ return { message: "Hello" };},
+- 方法屬性
+  - methods: { sum: function() { return this.price * this.quantity; },},
+  - methods: { sum_ES6() { return this.price * this.quantity; },},
+- 計算屬性
+  - computed: { sum_cpt: function(){ this.price * this.quantity; },},
+  - computed: { sum_cpt_ES6(){ this.price * this.quantity; },},
+  - computed: { sum_cpt_val{ get(){ return this.val1 + this.val2; }, set(){ this.val1 = this.val2*0.278; }}}
+- 樣板屬性
+  - template: `<div> {{ greeting }} 好棒棒!</div>`,
+  - data() {return { greeting: "Hello Vue.js!" };},
+
+## 常用指令(directive)
+- {{ data }}
+- v-bind-{屬性} or :{屬性}
+  - 包含style/class/src/id等
+- v-model
+  - 常用來雙向綁定data狀態
+- v-once
+  - 僅在指定節點上渲染一次
+- v-on:{事件} or @{事件}
+  - 事件綁定 常見 @click 用來傳遞JS事件物件(Event Object)、參數等
+- v-text
+  - 將輸出渲染成純文字
+- v-html
+  - 將輸出渲染成HTML格式
+- v-pre
+  - 取消Vue將 {{ data }} 視為模板語法並作為純文字輸出
+
+## v-model 修飾子 (Modifiers) 
+- .lazy 
+  - 於焦點離開輸入框時改變狀態 如同change事件
+- .number 
+  - 由 DOM API 取出的時候都會是以「字串」的形式出現
+  - 於更新狀態時會在背後試圖將對應資料轉型為數值格式
+- .trim 
+  - 於更新狀態時自動去除前後空白字元
+
+
+## 事件通用修飾子
+- .stop
+  - 阻擋事件向外層冒泡傳遞
+- .prevent
+  - 阻擋元素的預設事件(取消原事件)
+- .self
+- .once
+- .passive
+
